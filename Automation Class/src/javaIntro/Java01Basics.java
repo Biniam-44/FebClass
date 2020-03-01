65d936da471cb78bec818b2855b6e9c166caa16c
TechnoCraftCons <44950527+TechnoCraftCons@users.noreply.github.com>
2020-02-29 21:42:42 -0600

    Adding Java Basics

---
+++ Automation Class/src/javaIntro/Java01Basics.java
@@ -0,0 +1,10 @@
+package javaIntro;
+
+public class Java01Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Automation Class/src/javaIntro/Java02Methods.java
@@ -0,0 +1,52 @@
+package javaIntro;
+
+public class Java02Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * How we create an object is className name = new className();
+		 */
+
+		Java02Methods cm = new Java02Methods();
+		cm.AddTwoNumbers();
+		cm.SubTwoNumbers();
+		System.out.println(cm.MethodWithReturnType());
+
+	}
+
+	int a = 27;
+	int b = 18;
+
+	public void AddTwoNumbers() {
+		int sum = a + b;
+		System.out.println(sum);
+	}
+
+	public void SubTwoNumbers() {
+		int sub = a - b;
+		System.out.println(sub);
+	}
+
+	// Method with an Integer return type
+	public int MethodWithReturnType() {
+		return 2;
+	}
+
+	// Method with a boolean return type
+	public boolean MethodWithBoolReturn() {
+		return false;
+	}
+
+	public void MethodWithParameter(int a, int b) {
+
+		int sum = a + b;
+		System.out.println("My sum from the parameter is " + sum);
+	}
+
+	public void MethodWithStringParmeter(String name) {
+
+		System.out.println("My name is " + name);
+	}
+
+}
